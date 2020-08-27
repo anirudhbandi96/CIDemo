3b02d3874b5ad076cc32044dc93c8a0ff58ae89f
anirudhbandi96 <anirudhbandi96@gmail.com>
2020-08-27 13:11:38 -0400

    First Commit

---
+++ CIDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CIDemo
+//
+//  Created by Anirudh Bandi on 8/27/20.
+//  Copyright © 2020 Anirudh Bandi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CIDemoTests/CIDemoTests.swift
@@ -0,0 +1,34 @@
+//
+//  CIDemoTests.swift
+//  CIDemoTests
+//
+//  Created by Anirudh Bandi on 8/27/20.
+//  Copyright © 2020 Anirudh Bandi. All rights reserved.
+//
+
+import XCTest
+@testable import CIDemo
+
+class CIDemoTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
